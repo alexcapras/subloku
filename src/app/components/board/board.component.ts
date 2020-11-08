7704e01432a3de8dd5308e60f116fc5ae3f138cc
Alex Capras <alexcapras@gmail.com>
2020-11-08 17:16:05 +0000

    unused imports

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import * as constants from '../../utils/constants';
 
 import {
